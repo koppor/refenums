6a47a655c5faa1fcd18e0140e4cdb09b571425e5
Oliver Kopp <kopp.dev@gmail.com>
2013-12-16 22:05:28 +0100

    Removes release.sh from the released ZIP

--- release.sh
+++ release.sh
@@ -7,11 +7,12 @@ git archive -o refenums.zip --prefix=refenums/ HEAD
 #Delete ".gitignore". This files is not wanted by CTAN
 #In the future, other files starting with "." might be added, therefore we delete all files starting with "."
 zip -d refenums.zip /refenums/\.*
+zip -d refenums.zip /refenums/release.sh
 
 #Generate demo.pdf
-pdflatex demo
-pdflatex demo
-pdflatex demo
+#pdflatex demo
+#pdflatex demo
+#pdflatex demo
 
 #Add demo.pdf to the archive
 mkdir refenums
