63cf837290d46307d0f0ad0a38947816e96253c0
Oliver Kopp <kopp.dev@gmail.com>
2013-12-16 21:49:31 +0100

    Adds release script to make CTAN conforming refenums.zip

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#This script releases refenums.sty in the format required by CTAN
+
+#First create ZIP file containing versioned files only
+git archive -o refenums.zip --prefix=refenums/ HEAD
+
+#Delete ".gitignore". This files is not wanted by CTAN
+#In the future, other files starting with "." might be added, therefore we delete all files starting with "."
+zip -d refenums.zip /refenums/\.*
+
+#Generate demo.pdf
+pdflatex demo
+pdflatex demo
+pdflatex demo
+
+#Add demo.pdf to the archive
+mkdir refenums
+cp demo.pdf refenums
+zip -g refenums.zip refenums/demo.pdf
+rm refenums/demo.pdf
