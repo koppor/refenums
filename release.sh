89eac61b3ba6f10d9570a00d3798c3c9225850ae
Oliver Kopp <kopp.dev@gmail.com>
2016-10-07 20:29:26 +0200

    Refine release.sh to include a PDF version of README.md

--- release.sh
+++ release.sh
@@ -4,18 +4,20 @@
 #First create ZIP file containing versioned files only
 git archive -o refenums.zip --prefix=refenums/ HEAD
 
-#Delete ".gitignore". This files is not wanted by CTAN
+#Delete ".gitignore" and "release.sh". These files is not wanted by CTAN
 #In the future, other files starting with "." might be added, therefore we delete all files starting with "."
-zip -d refenums.zip /refenums/\.*
-zip -d refenums.zip /refenums/release.sh
+zip refenums.zip -d /refenums/\.*
+zip refenums.zip -d ./refenums/release.sh
 
 #Generate demo.pdf
 pdflatex demo
 pdflatex demo
 pdflatex demo
 
-#Add demo.pdf to the archive
+#Generate README.pdf
+pandoc -f markdown_github-hard_line_breaks -t latex README.md -o README.pdf
+
+#Add demo.pdf and README.pdf to the archive
 mkdir refenums
-cp demo.pdf refenums
-zip -g refenums.zip refenums/demo.pdf
-rm refenums/demo.pdf
+cp demo.pdf README.pdf refenums
+zip -g refenums.zip refenums/demo.pdf refenums/README.pdf
